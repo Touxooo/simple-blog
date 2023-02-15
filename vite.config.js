2c2d2781cc5577603b91485533fa2ff6a92b9306
Touxooo <thomas.rabiet@epitech.eu>
2023-02-15 11:29:22 +0100

    [ADD] article page nice

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -7,4 +10,5 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 5173, // you can replace this port with any port
-  }
+  },
+});
