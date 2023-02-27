88729c9c9a8ebea63b583113aa177b25ffeb664a
Touxooo <thomas.rabiet@epitech.eu>
2023-02-27 15:55:00 +0100

    [ADD] React Hook Form Logic

--- src/components/articles/createForm/index.jsx
+++ src/components/articles/createForm/index.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import "./index.css";
 
+import { useForm } from "react-hook-form";
+
 const CreateArticleForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    window.alert(JSON.stringify(data));
+  };
+
   return (
-    <form className="create-article-form-container">
+    <form
+      className="create-article-form-container"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <input
         className="create-article-form-input"
         type="text"
         placeholder="Title"
+        {...register("title")}
       />
       <input
         className="create-article-form-input"
         type="text"
         placeholder="Description"
+        {...register("description")}
       />
-      <input
-        className="create-article-form-input"
+      <textarea
+        className="create-article-form-input body"
         type="text"
         placeholder="Body"
+        rows={10}
+        {...register("body")}
       />
       <input
         className="create-article-form-input"
         type="text"
         placeholder="Author"
+        {...register("author")}
       />
       <button className="create-article-form-button" type="submit">
         Submit article
