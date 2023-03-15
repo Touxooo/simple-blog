68daddb85d63039c81b2fdaf971f5ae34f78b85b
Touxooo <thomas.rabiet@epitech.eu>
2023-03-15 17:20:25 +0100

    [MOD] Use App to show Page + Articles Body

--- src/constants/articles.js
+++ src/constants/articles.js
@@ -1,67 +1,104 @@
 const articles = [
   {
     id: 0,
-    title: "Article 1",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
+    title: "L'impact de l'intelligence artificielle sur les emplois",
+    body: `L'essor de l'intelligence artificielle (IA) a suscité des craintes quant à son impact sur les emplois. Les machines pourraient-elles remplacer les travailleurs humains ? Quels emplois pourraient être affectés ?
+
+Les experts en IA prédisent que les machines remplaceront certains emplois, mais qu'elles en créeront également de nouveaux. Les emplois qui nécessitent des tâches répétitives et standardisées, telles que l'assemblage en usine, sont les plus susceptibles d'être remplacés par des machines. Les emplois qui impliquent une interaction humaine, tels que les professions de santé, les services de restauration et les emplois créatifs, sont moins susceptibles d'être remplacés.
+
+Cependant, l'impact potentiel de l'IA sur les emplois est complexe. Les machines pourraient augmenter la productivité et réduire les coûts, ce qui pourrait stimuler la création d'emplois dans d'autres secteurs. Cependant, la transition vers une économie axée sur l'IA pourrait prendre du temps, et certains travailleurs pourraient avoir besoin de se requalifier pour trouver du travail dans de nouveaux domaines.
+
+En fin de compte, il est difficile de prédire l'impact exact de l'IA sur les emplois. Cependant, il est clair que les entreprises et les gouvernements doivent travailler ensemble pour aider les travailleurs à se préparer à la transition vers une économie axée sur l'IA, notamment en fournissant des programmes de formation et de développement des compétences.`,
     author: "John Doe",
     creationDate: "21 Jan 2019",
     readMinuteTime: 5,
   },
   {
     id: 1,
-    title: "Article 2",
-    body: "Amet risus nullam eget felis eget nunc lobortis. Euismod quis viverra nibh cras pulvinar mattis nunc sed blandit. Enim praesent elementum facilisis leo vel fringilla est. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Diam vel quam elementum pulvinar etiam non quam lacus suspendisse. Risus sed vulputate odio ut enim blandit. Eu facilisis sed odio morbi quis commodo odio. Fermentum iaculis eu non diam phasellus. Sit amet nisl suscipit adipiscing bibendum. Fringilla est ullamcorper eget nulla facilisi etiam dignissim diam.",
+    title: "Les tendances actuelles de la cybersécurité",
+    body: `La cybersécurité est devenue une préoccupation majeure pour les entreprises et les particuliers, car les menaces de cyberattaques sont de plus en plus courantes et sophistiquées. Voici quelques-unes des tendances actuelles de la cybersécurité :
+- Les attaques de ransomware : les attaques de ransomware sont devenues plus courantes ces dernières années. Les cybercriminels utilisent des logiciels malveillants pour chiffrer les données des victimes, puis exigent une rançon pour les déchiffrer.
+- Les menaces de phishing : les cybercriminels utilisent des e-mails et des messages de texte frauduleux pour inciter les victimes à divulguer des informations sensibles, telles que des identifiants de connexion ou des informations bancaires.
+- Les attaques DDoS : les attaques DDoS (déni de service distribué) sont des attaques qui surchargent un site web avec du trafic, le rendant inaccessible pour les utilisateurs légitimes.
+    
+Pour se protéger contre ces menaces, il est important de prendre des mesures de sécurité telles que l'utilisation de mots de passe forts, la mise à jour régulière des logiciels et l'installation de logiciels antivirus et anti-malware. Les entreprises doivent également mettre en place des politiques de sécurité solides et former leur personnel à reconnaître et à éviter les menaces de cybersécurité.`,
     author: "John Doe",
     creationDate: "23 Jan 2019",
     readMinuteTime: 7,
   },
   {
     id: 2,
-    title: "Article 3",
-    body: "Nisi quis eleifend quam adipiscing vitae. Mauris in aliquam sem fringilla ut. Elementum eu facilisis sed odio morbi. Morbi non arcu risus quis varius quam. Nulla facilisi etiam dignissim diam quis. Nam aliquam sem et tortor consequat. Lacus laoreet non curabitur gravida arcu. Consequat nisl vel pretium lectus quam. Duis at consectetur lorem donec massa sapien. Ultrices gravida dictum fusce ut. Nunc id cursus metus aliquam eleifend mi in nulla posuere. Lacus suspendisse faucibus interdum posuere lorem ipsum dolor. Ultrices gravida dictum fusce ut placerat orci nulla. Ultrices mi tempus imperdiet nulla malesuada. Consectetur adipiscing elit pellentesque habitant morbi tristique. Quam vulputate dignissim suspendisse in est ante in nibh. Pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat. Venenatis urna cursus eget nunc scelerisque viverra mauris.",
+    title: "Les avantages et les inconvénients de la réalité virtuelle",
+    body: `La réalité virtuelle (RV) est une technologie qui permet aux utilisateurs de vivre une expérience immersive dans un environnement virtuel. Voici quelques-uns des avantages et des inconvénients de la RV :
+
+Avantages :
+
+- Amélioration de l'expérience utilisateur : la RV peut offrir une expérience immersive et réaliste qui peut améliorer l'expérience utilisateur dans de nombreux domaines, tels que les jeux vidéo, la formation professionnelle et la santé mentale.
+- Possibilités éducatives : la RV peut être utilisée pour offrir des possibilités éducatives uniques, telles que la visite de sites historiques ou la simulation de situations de crise.
+- Innovation dans la recherche : la RV peut être utilisée pour simuler des expériences et des environnements qui seraient autrement impossibles ou dangereux à reproduire dans la vie réelle.
+
+Inconvénients :
+
+- Risques pour la santé : la RV peut causer des nausées, des maux de tête et d'autres symptômes chez certaines personnes. Il est important de s'assurer que l'environnement virtuel est sûr et confortable pour les utilisateurs.
+- Coût élevé : la technologie de RV est encore relativement nouvelle et coûteuse, ce qui peut limiter son accessibilité.
+- Risques pour la santé mentale : la RV peut avoir des implications pour la santé mentale, notamment en créant une dépendance ou en exacerbant les symptômes de certaines maladies mentales.
+
+En fin de compte, la RV présente des avantages et des inconvénients, et il est important d'envisager ces facteurs lors de la décision d'utiliser cette technologie dans un contexte particulier.`,
     author: "John Doe",
     creationDate: "25 Jan 2019",
     readMinuteTime: 4,
   },
   {
     id: 3,
-    title: "Article 4",
-    body: "Non sodales neque sodales ut etiam. Ullamcorper malesuada proin libero nunc consequat. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Odio tempor orci dapibus ultrices. Eget mi proin sed libero enim. Urna et pharetra pharetra massa. At urna condimentum mattis pellentesque id. Ut sem viverra aliquet eget. Quis vel eros donec ac odio tempor orci. Venenatis tellus in metus vulputate eu scelerisque. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.",
+    title: "Les technologies émergentes dans l'industrie automobile",
+    body: `L'industrie automobile a connu une croissance rapide ces dernières années, avec de nouvelles technologies qui ont changé la façon dont nous concevons, fabriquons et utilisons les voitures. Voici quelques-unes des technologies émergentes dans l'industrie automobile :
+
+- Voitures autonomes : les voitures autonomes sont des voitures qui peuvent fonctionner sans intervention humaine. Les progrès dans la technologie de la conduite autonome pourraient réduire le nombre d'accidents de la route et offrir une plus grande flexibilité aux conducteurs.
+- Technologies de sécurité avancées : les technologies de sécurité avancées, telles que les systèmes de freinage automatique et les avertissements de sortie de voie, peuvent aider à prévenir les accidents de la route.
+- Innovations dans la propulsion électrique : les innovations dans la propulsion électrique ont permis de créer des voitures plus écologiques et plus efficaces sur le plan énergétique.
+
+Cependant, ces technologies émergentes peuvent également présenter des défis, tels que la sécurité et la réglementation. Il est important que les entreprises et les gouvernements travaillent ensemble pour garantir que ces technologies sont développées de manière responsable et sûre.`,
     author: "John Doe",
     creationDate: "28 Jan 2019",
     readMinuteTime: 3,
   },
   {
     id: 4,
-    title: "Article 5",
-    body: "Non sodales neque sodales ut etiam. Ullamcorper malesuada proin libero nunc consequat. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Odio tempor orci dapibus ultrices. Eget mi proin sed libero enim. Urna et pharetra pharetra massa. At urna condimentum mattis pellentesque id. Ut sem viverra aliquet eget. Quis vel eros donec ac odio tempor orci. Venenatis tellus in metus vulputate eu scelerisque. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.",
+    title: "Les défis de la transition vers une énergie propre",
+    body: `La transition vers une énergie propre est devenue une priorité majeure pour de nombreux pays, car ils cherchent à réduire leur dépendance aux combustibles fossiles et à réduire leur impact environnemental. Cependant, cette transition présente des défis importants, tels que :
+
+- La dépendance continue aux combustibles fossiles : les combustibles fossiles restent la principale source d'énergie dans de nombreux pays, et la transition vers une énergie propre peut être difficile sans une réduction de la consommation de combustibles fossiles.
+- Les coûts élevés de l'énergie renouvelable : les technologies énergétiques renouvelables sont souvent plus coûteuses que les combustibles fossiles, ce qui peut limiter leur adoption.
+- Obstacles réglementaires et politiques : les politiques et les réglementations peuvent parfois ralentir la transition vers une énergie propre, en freinant les investissements dans les technologies énergétiques renouvelables ou en favorisant les combustibles fossiles.
+
+Pour surmonter ces défis, il est important de s'engager dans une planification et une coordination à long terme, de fournir des incitations financières pour encourager la transition vers une énergie propre et de travailler avec les parties prenantes pour surmonter les obstacles réglementaires et politiques.`,
     author: "John Doe",
     creationDate: "28 Jan 2019",
     readMinuteTime: 12,
   },
   {
     id: 5,
-    title: "Article 6",
-    body: "Non sodales neque sodales ut etiam. Ullamcorper malesuada proin libero nunc consequat. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Odio tempor orci dapibus ultrices. Eget mi proin sed libero enim. Urna et pharetra pharetra massa. At urna condimentum mattis pellentesque id. Ut sem viverra aliquet eget. Quis vel eros donec ac odio tempor orci. Venenatis tellus in metus vulputate eu scelerisque. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.",
+    title: "L'avenir des ordinateurs quantiques",
+    body: `Les ordinateurs quantiques sont une technologie émergente qui pourrait révolutionner l'informatique, en offrant des capacités de traitement beaucoup plus rapides que les ordinateurs traditionnels. Les avancées récentes dans les ordinateurs quantiques ont montré leur potentiel pour résoudre des problèmes complexes dans des domaines tels que la finance, la science et la sécurité.
+
+Cependant, il reste encore beaucoup à faire pour développer cette technologie, notamment en surmontant les défis tels que la stabilité des qubits, la réduction des erreurs et la création de nouveaux algorithmes. À mesure que cette technologie se développe, elle pourrait avoir des implications importantes pour l'informatique et les industries qui en dépendent.`,
     author: "John Doe",
     creationDate: "28 Jan 2019",
     readMinuteTime: 18,
   },
   {
     id: 6,
-    title: "Article 7",
-    body: "Non sodales neque sodales ut etiam. Ullamcorper malesuada proin libero nunc consequat. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Odio tempor orci dapibus ultrices. Eget mi proin sed libero enim. Urna et pharetra pharetra massa. At urna condimentum mattis pellentesque id. Ut sem viverra aliquet eget. Quis vel eros donec ac odio tempor orci. Venenatis tellus in metus vulputate eu scelerisque. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.",
+    title: "Les innovations dans la technologie alimentaire",
+    body: `Les innovations dans la technologie alimentaire ont permis de créer de nouveaux aliments et de nouvelles façons de produire, de distribuer et de consommer des aliments. Voici quelques-unes des innovations actuelles dans la technologie alimentaire :
+
+- Aliments imprimés en 3D : les aliments imprimés en 3D permettent de créer des formes et des textures uniques pour les aliments.
+- Alternatives végétales à la viande : les alternatives végétales à la viande, telles que les hamburgers végétaliens, gagnent en popularité auprès des consommateurs soucieux de leur santé et de l'environnement.
+- Aliments personnalisés en fonction de l'ADN du consommateur : les avancées dans la technologie de la génomique ont permis de créer des aliments personnalisés en fonction de l'ADN du consommateur, ce qui peut améliorer la santé et la nutrition.
+
+Cependant, ces innovations peuvent également présenter des défis, tels que la sécurité alimentaire et la réglementation. Il est important que les entreprises et les gouvernements travaillent ensemble pour garantir que ces technologies sont développées de manière responsable et sûre.`,
     author: "John Doe",
     creationDate: "28 Jan 2019",
     readMinuteTime: 5,
-  },
-  {
-    id: 5,
-    title: "Article 8",
-    body: "Non sodales neque sodales ut etiam. Ullamcorper malesuada proin libero nunc consequat. Volutpat blandit aliquam etiam erat velit scelerisque in dictum. Odio tempor orci dapibus ultrices. Eget mi proin sed libero enim. Urna et pharetra pharetra massa. At urna condimentum mattis pellentesque id. Ut sem viverra aliquet eget. Quis vel eros donec ac odio tempor orci. Venenatis tellus in metus vulputate eu scelerisque. Luctus accumsan tortor posuere ac ut consequat semper viverra nam. Fames ac turpis egestas maecenas pharetra convallis posuere morbi.",
-    author: "John Doe",
-    creationDate: "28 Jan 2019",
-    readMinuteTime: 8,
   },
 ];
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 import "./index.css";
-import ArticlePage from "./pages/article";
-import CreateArticlePage from "./pages/createArticle";
-import HomePage from "./pages/home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CreateArticlePage />
+    <App />
   </React.StrictMode>
 );
